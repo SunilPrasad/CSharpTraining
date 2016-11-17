6fbfce461be184582db699c160c7a97bc2ad9f56
sunil.hbti@gmail.com <sunil.hbti@gmail.com>
2016-11-17 22:23:01 +0530

    SerilizationExample

---
+++ CSharpTraining/XmlAttributeExample/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XmlAttributeExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    //public class AddressDetails
+    //{
+    //    [XmlElement("Number")]
+    //    public int HouseNo { get; set; }
+    //    [XmlElement("Street")]
+    //    public string StreetName { get; set; }
+    //    [XmlElement("City")]
+    //    public string CityName { get; set; }
+    //}
+
+    //public class AddresssDetails
+    //{
+    //    [XmlAttribute("Number")]
+    //    public int HouseNo { get; set; }
+    //    [XmlElement("Street")]
+    //    public string StreetName { get; set; }
+    //    [XmlElement("CityName")]
+    //    public string City { get; set; }
+    //}
+
+    //public class AddressDetails
+    //{
+    //    [XmlElement("Number")]
+    //    public int HouseNo;
+    //    [XmlElement("Street")]
+    //    public string StreetName;
+    //    [XmlIgnore]
+    //    public string City;
+    //}
+
+    //[XmlRoot("Root")]
+    //public class AddressDetails
+    //{
+    //    [XmlElement("Number")]
+    //    public int HouseNo;
+    //    [XmlElement("Street")]
+    //    public string StreetName;
+    //    [XmlElement("CityName")]
+    //    public string City;
+    //}
+}
---
+++ CSharpTraining/XmlSerilizationExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace XmlSerilizationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            AddressDetails details = new AddressDetails();
+            details.HouseNo = 4;
+            details.StreetName = "Rohini";
+            details.City = "Delhi";
+            
+
+
+            Serialize(details);
+        }
+
+        public static void Serialize(AddressDetails details)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(AddressDetails));
+
+            TextWriter writer = new StreamWriter(@"Address.xml");
+
+            serializer.Serialize(writer, details);
+
+        }
+    }
+
+    public class AddressDetails
+    {
+        public int HouseNo { get; set; }
+        public string StreetName { get; set; }
+        public string City { get; set; }
+        private string PoAddress { get; set; }
+    }
+}
